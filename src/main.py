6a33737367ee164c2c0b3ebdad1db19f5483d92b
alexi <c3073816@newcastle.ac.uk>
2026-09-10 05:50:50 +0200

    Made a main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import sys
+from get_weather import get_weather
+from clean_weather_file import clean_weather_file
+from weather_mapping import weather_mapping
+from create_midi import create_midi
+
+def main():
+    get_weather()
+    clean_weather_file()
+    weather_mapping()
+    create_midi()
+
+if __name__ == '__main__':
+    main()
